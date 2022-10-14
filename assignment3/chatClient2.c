4280efbad0448a1063b661c991241ea9c3f8061d
Rob Clancy <clanrob@chrobinson.com>
2022-10-14 16:00:15 -0500

    sf

--- assignment3/chatClient2.c
+++ assignment3/chatClient2.c
@@ -245,11 +245,11 @@ void registerclient() {
             char* token = strtok(host_space_port, " ");
             printf("%s\n", token);
             strcpy(hostcopy, token);
-            printf("%s\n", hostcopy);
             printf("hey\n");
             token = strtok(NULL, " ");
             token = strtok(host_space_port, " ");
-            printf("hey\n");
+            printf("%s\n", token);
+
             strcpy(port, token);
             printf("%s", port);
             if (port == NULL) {
