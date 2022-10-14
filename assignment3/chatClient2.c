41ca9012230ce9262565919c6ed2c808055db597
Rob Clancy <clanrob@chrobinson.com>
2022-10-14 17:40:56 -0500

    sfd

--- assignment3/chatClient2.c
+++ assignment3/chatClient2.c
@@ -284,10 +284,10 @@ char* selection(int selection, char* text) {
     const char s[2] = " ";
     const char ch = '"';
     char *substring;
-    char *token;
     substring = strrchr(text, ch);
     printf(" substring %s\n", substring);
-    token = strtok(substring, s);
+    char* token = strtok(substring, s);
+    printf(" token %s\n", token);
     int i = 0;
     while (token != NULL)
     {
